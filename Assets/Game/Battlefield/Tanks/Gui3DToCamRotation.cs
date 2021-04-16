1741cdd7dee73e5a47865f94ba6b9d0d4224cb5a
pliersch <hourby@gmail.com>
2021-04-16 10:38:12 +0200

    - rotate 3d gui elements to camera

---
+++ Assets/Game/Battlefield/Tanks/Gui3DToCamRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game.Battlefield.Tanks {
+
+	public class Gui : MonoBehaviour {
+
+		public GameObject _text;
+		
+		private void Update() {
+			ComputeDirection();
+		}
+
+		private void ComputeDirection() {
+			Vector3 dir = Quaternion.Euler(0, 270, 45) * Vector3.down;
+			Quaternion rotation = Quaternion.LookRotation(dir);
+			_text.transform.rotation = rotation;
+		}
+
+	}
+
+}
